b69c71775343760d6fbd14dbe618e9bc281033eb
Edu Iglesias <eiglesiash@gmail.com>
2021-09-22 22:57:18 +0200

    Creado el topbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Menu from './components/Menu/Menu'
+import TopBar from './components/TopBar/TopBar';
 
 function App() {
   return (
     <>
       <Menu />
+      <TopBar/>
       <main className="Main">
         <h1 className="App__title">Agora | Tu app de gestión de bibliotecas</h1>
         <Dashboard />
---
+++ frontend/src/components/TopBar/TopBar.js
@@ -0,0 +1,9 @@
+import './TopBar.css';
+
+export default function TopBar(){
+  return (
+    <section className="TopBar">
+      <p>Esto es el topbar</p>
+    </section>
+  )
+}

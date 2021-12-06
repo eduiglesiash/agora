34a48347aec9848ee90c56ff92f56ca7b5712805
David Casarrubio Ramírez <dcasaram@gmail.com>
2021-12-06 19:28:26 +0100

    Avances en formulario de libros

--- frontend/src/pages/Books/Books.page.js
+++ frontend/src/pages/Books/Books.page.js
@@ -4,149 +4,15 @@ import Book from '../../components/Book/Book'
 import { useState, useEffect } from 'react'
 import axios from 'axios';
 
+import BooksModal from './BooksModal/BooksModal';
+
 const booksApi = `https://www.googleapis.com/books/v1/volumes?q=isbn:`;
 const booksApiOpts = `&maxResults=1`;
-
-const Input = ({ id, classes, value, name, type, layer, placeholder, onChange, error, errorHint }) => {
-  const classnames = `a-input ${classes}`;
-  return (
-    <div className='a-input__container'>
-      <span className='a-input__layer'>{layer}</span>
-      <input
-        id={id}
-        className={classnames}
-        value={value}
-        name={name}
-        type={type}
-        placeholder={placeholder}
-        onChange={onChange}
-      />
-      { error && (
-        <span class='a-input__error'>{errorHint}</span>
-      )}
-    </div>
-  )
-}
-
-const Textarea = ({ id, classes, value, name, type, layer, placeholder, onChange, error, errorHint }) => {
-  const classnames = `a-input ${classes}`;
-  return (
-    <div className='a-input__container'>
-      <span className='a-input__layer'>{layer}</span>
-      <textarea
-        id={id}
-        className={classnames}
-        value={value}
-        name={name}
-        type={type}
-        placeholder={placeholder}
-        onChange={onChange}
-      />
-      { error && (
-        <span class='a-input__error'>{errorHint}</span>
-      )}
-    </div>
-  )
-}
-
-const BookModal = ({ formValues, onChangeInput, searchIsbn, saveBook, closeModal }) => {
-  return (
-    <section className='Book__modal a-flex a-flex-center'>
-      <div className='Book__modal-content'>
-        <header className='Book__modal-header'>
-          <h2>Añadir un nuevo libro</h2>
-        </header>
-        <form className='Book__modal-form'>
-          <Input
-            id='isbn'
-            name='isbn'
-            type='text'
-            layer='Buscar por ISBN'
-            value={formValues.isbn}
-            placeholder='ISBN'
-            onChange={onChangeInput}
-            error={false}
-            errorHint='Mensaje error'
-          />
-          <button className='a-cta Book__cta' type='button' onClick={searchIsbn}>Buscar por ISBN</button>
-          <p className='a-fs-18 a-fw-700 Book__paragraph'>En caso que no se encuentre la información a través del ISBN o no dispongas del mismo, introduce los datos:</p>
-          <Input
-            id='title'
-            name='title'
-            type='text'
-            layer='Título'
-            value={formValues.title}
-            placeholder='Título del libro'
-            onChange={onChangeInput}
-            error={false}
-            errorHint='Mensaje error'
-          />
-          <Input
-            id='author'
-            name='author'
-            type='text'
-            layer='Autor'
-            value={formValues.author}
-            placeholder='Autor del libro'
-            onChange={onChangeInput}
-            error={false}
-            errorHint='Mensaje error'
-          />
-          <Input
-            id='image'
-            name='image'
-            type='text'
-            layer='Imagen'
-            value={formValues.image}
-            placeholder='Url de la imagen del libro'
-            onChange={onChangeInput}
-            error={false}
-            errorHint='Mensaje error'
-          />
-          <Input
-            id='categories'
-            name='categories'
-            type='text'
-            layer='Categorías'
-            value={formValues.categories}
-            placeholder='Categorías a las que pertenece el libro'
-            onChange={onChangeInput}
-            error={false}
-            errorHint='Mensaje error'
-          />
-          <Textarea
-            id='description'
-            classes='Book__description'
-            name='description'
-            layer='Descripción'
-            value={formValues.description}
-            placeholder='Descripción del libro'
-            onChange={onChangeInput}
-            error={false}
-            errorHint='Mensaje error'
-          />
-          <Input
-            id='quantity'
-            name='quantity'
-            type='number'
-            layer='Cantidad'
-            value={formValues.quantity}
-            placeholder='Cantidad de libros'
-            onChange={onChangeInput}
-            error={false}
-            errorHint='Mensaje error'
-          />
-          <button className='a-cta Book__cta' type='button' onClick={saveBook}>Guardar libro</button>
-        </form>
-        <button className='Book__modal-close' onClick={closeModal}><span class='a-visually-hidden'>Cerrar modal</span></button>
-      </div>
-    </section>
-  )
-}
 
 export default function BooksPage() {
   const [books, setBooks] = useState([])
   const [modal, setModal] = useState(false);
+  const [loadingForm, setLoadingForm] = useState(false);
   const [formValues, setFormValues] = useState({
     isbn: '',
     title: '',
@@ -154,7 +20,24 @@ export default function BooksPage() {
     image: '',
     categories: '',
     description: '',
-    quantity: null,
+    quantity: undefined,
+  });
+  const [formErrors, setFormErrors] = useState({
+    title: '',
+    author: '',
+    categories: '',
+    description: '',
+    quantity: '',
+  });
+
+  const clearForm = () => setFormValues({
+    isbn: '',
+    title: '',
+    author: '',
+    image: '',
+    categories: '',
+    description: '',
+    quantity: undefined,
   });
 
   const onChangeInput = (e) => {
@@ -176,23 +59,66 @@ export default function BooksPage() {
     }, '');
     const obj = {
       title: res.volumeInfo.title,
-      author: authors,
-      image: res.volumeInfo.imageLinks.thumbnail,
-      categories: categories,
+      author: authors.substring(0, authors.length - 2),
+      image: res.volumeInfo.imageLinks?.thumbnail,
+      categories: categories.substring(0, categories.length - 2),
       description: res.volumeInfo.description,
     }
     setFormValues({ ...formValues, ...obj });
-    console.log(formValues);
+    setLoadingForm(false);
   }
 
   const searchIsbn = () => {
+    clearForm();
+    setLoadingForm(true);
     axios
       .get(`${booksApi}${formValues.isbn}${booksApiOpts}`)
-      .then((res) => updateFormData({ res: res.data.items[0] }));
+      .then((res) => {
+        if (res.data.totalItems === 0) {
+          setLoadingForm(false);
+          return alert('ISBN NO ENCONTRADO, INTRODUCIR LOS DATOS EN EL FORMULARIO');
+        }
+        return updateFormData({ res: res.data.items[0] })
+      });
   }
 
-  const saveBook = () => {
+  const isFormCorrect = () => {
+    let error = false;
+    let obj = {
+      title: '',
+      author: '',
+      categories: '',
+      description: '',
+      quantity: '',
+    };
+    if (formValues.title === '') {
+      error = true;
+      obj = { ...obj, title: 'El título es obligatorio' };
+    }
+    if (formValues.author === '') {
+      error = true;
+      obj = { ...obj, author: 'El autor es obligatorio' };
+    }
+    if (formValues.categories === '') {
+      error = true;
+      obj = { ...obj, categories: 'Es obligatirio incluir al menos una categoría' };
+    }
+    if (formValues.description === '') {
+      error = true;
+      obj = { ...obj, description: 'La descripción es obligatoria' };
+    }
+    if (formValues.quantity === undefined || formValues.quantity === '') {
+      error = true;
+      obj = { ...obj, quantity: 'Es obligatorio introducir el número de libros que se disponen' };
+    }
+    setFormErrors(obj);
+    return !error;
+  }
 
+  const onSubmit = () => {
+    if (isFormCorrect()) {
+      console.log('guardar formulario!');
+    }
   }
 
   const toggleModal = () => {
@@ -212,15 +138,17 @@ export default function BooksPage() {
           isbn={book.isbn}
         />)
       }
-      <button type='button' className='Book__add' onClick={toggleModal}><span class='a-visually-hidden'>Añadir nuevo libro</span></button>
+      <button type='button' className='Book__add' onClick={toggleModal}><span className='a-visually-hidden'>Añadir nuevo libro</span></button>
       {
         modal && (
-          <BookModal
+          <BooksModal
             formValues={formValues}
+            formErrors={formErrors}
             onChangeInput={onChangeInput}
             searchIsbn={searchIsbn}
-            saveBook={saveBook}
             closeModal={toggleModal}
+            loading={loadingForm}
+            onSubmit={onSubmit}
           />
         )
       }
---
+++ frontend/src/pages/Books/BooksModal/BooksModal.js
@@ -0,0 +1,98 @@
+import BooksInput from './../BooksInput/BooksInput';
+import BooksTextarea from './../BooksTextarea/BooksTextarea';
+
+import './BooksModal.css';
+
+export default function BooksModal({ formValues, formErrors, onChangeInput, searchIsbn, onSubmit, closeModal, loading }) {
+    return (
+      <section className='Books__modal a-flex a-flex-center'>
+        <div className='Books__modal-content'>
+          <header className='Books__modal-header'>
+            <h2>Añadir un nuevo libro</h2>
+          </header>
+          <form className='Books__modal-form'>
+            <BooksInput
+              id='isbn'
+              name='isbn'
+              type='text'
+              layer='Buscar por ISBN'
+              value={formValues.isbn}
+              placeholder='ISBN'
+              onChange={onChangeInput}
+            />
+            <button className='a-cta Book__cta' type='button' onClick={searchIsbn}>Buscar por ISBN</button>
+            <p className='a-fs-18 a-fw-700 Book__paragraph'>En caso que no se encuentre la información a través del ISBN o no dispongas del mismo, introduce los datos:</p>
+            <BooksInput
+              id='title'
+              name='title'
+              type='text'
+              layer='Título'
+              value={formValues.title}
+              placeholder='Título del libro'
+              onChange={onChangeInput}
+              error={formErrors.title}
+            />
+            <BooksInput
+              id='author'
+              name='author'
+              type='text'
+              layer='Autor'
+              value={formValues.author}
+              placeholder='Autor del libro'
+              helpText='Si hay varios autores, separar por comas'
+              onChange={onChangeInput}
+              error={formErrors.author}
+            />
+            <BooksInput
+              id='image'
+              name='image'
+              type='text'
+              layer='Imagen'
+              value={formValues.image}
+              placeholder='Url de la imagen del libro'
+              helpText='Si no se dispone de imagen, dejar en blanco'
+              onChange={onChangeInput}
+            />
+            <BooksInput
+              id='categories'
+              name='categories'
+              type='text'
+              layer='Categorías'
+              value={formValues.categories}
+              placeholder='Categorías a las que pertenece el libro'
+              helpText='Si hay varias categorías, separar por comas'
+              onChange={onChangeInput}
+              error={formErrors.categories}
+            />
+            <BooksTextarea
+              id='description'
+              classes='Book__description'
+              name='description'
+              layer='Descripción'
+              value={formValues.description}
+              placeholder='Descripción del libro'
+              onChange={onChangeInput}
+              error={formErrors.description}
+            />
+            <BooksInput
+              id='quantity'
+              name='quantity'
+              type='number'
+              layer='Cantidad'
+              value={formValues.quantity}
+              placeholder='Cantidad de libros'
+              onChange={onChangeInput}
+              error={formErrors.quantity}
+            />
+            <button className='a-cta Book__cta' type='button' onClick={onSubmit}>Guardar libro</button>
+            {
+              loading && (
+                <span className='Books__modal-loading' aria-hidden='true'>Cargando datos...</span>
+              )
+            }
+          </form>
+          <button className='Books__modal-close' onClick={closeModal}><span className='a-visually-hidden'>Cerrar modal</span></button>
+        </div>
+      </section>
+    )
+}
